e3e26293cfce2c30abc673d87964c35a3fb08b49
Sergei Venukov <shid1095@gmail.com>
2022-03-04 12:36:36 +0500

    fix roles in skzi units router

--- server/src/skzi-units/skzi-units.router.ts
+++ server/src/skzi-units/skzi-units.router.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import { skziUnitsController } from './skzi-units.controller'
 import { authMiddleware } from '../middleware/auth.middleware'
-import { adminRolesMiddleware } from '../middleware/roles.middleware'
+import {
+  adminRolesMiddleware,
+  operatorRolesMiddleware,
+  userRolesMiddleware
+} from '../middleware/roles.middleware'
 
 export const skziUnitsRouter = Router()
 
-skziUnitsRouter.get('/', authMiddleware, adminRolesMiddleware(), skziUnitsController.getAll)
-skziUnitsRouter.get('/:id', authMiddleware, adminRolesMiddleware(), skziUnitsController.getById)
-skziUnitsRouter.post('/', authMiddleware, adminRolesMiddleware(), skziUnitsController.create)
+skziUnitsRouter.get('/', authMiddleware, userRolesMiddleware(), skziUnitsController.getAll)
+skziUnitsRouter.get('/:id', authMiddleware, userRolesMiddleware(), skziUnitsController.getById)
+skziUnitsRouter.post('/', authMiddleware, operatorRolesMiddleware(), skziUnitsController.create)
 skziUnitsRouter.patch('/:id', authMiddleware, adminRolesMiddleware(), skziUnitsController.update)
 skziUnitsRouter.delete('/:id', authMiddleware, adminRolesMiddleware(), skziUnitsController.remove)
