a0b5306a439ef307147eb92d0e85fea63f6cf512
Sergei Venukov <shid1095@gmail.com>
2022-03-30 22:31:09 +0500

    split routes and menu items into different components

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,22 +1,66 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
-import { Page } from '../interfaces/Page'
+import {
+  TeamOutlined,
+  SnippetsOutlined,
+  FileDoneOutlined,
+  LockOutlined
+} from '@ant-design/icons'
+import { selectAuth } from '../store/auth/auth.slice'
+import useTypedSelector from '../hooks/useTypedSelector'
+import isRoleMatch from '../utils/isRoleMatch'
+import { Roles } from '../enums/Roles'
 
 interface SidebarProps {
   collapsed: boolean
-  pages: Page[]
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ collapsed, pages }) => {
+interface MenuItem {
+  path: string
+  label: string
+  icon: React.ReactNode
+  role?: Roles
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
+  const { user } = useTypedSelector(selectAuth)
   const location = useLocation()
+
+  const menu = useMemo(() => {
+    const items: MenuItem[] = [
+      {
+        path: '/users',
+        label: 'Пользователи',
+        icon: <TeamOutlined />,
+        role: Roles.Admin
+      },
+      {
+        path: '/acts',
+        label: 'Акты',
+        icon: <SnippetsOutlined />
+      },
+      {
+        path: '/agreements',
+        label: 'Соглашения',
+        icon: <FileDoneOutlined />
+      },
+      {
+        path: '/skzi-units',
+        label: 'СКЗИ',
+        icon: <LockOutlined />
+      }
+    ]
+
+    return items.filter(item => user && (!item.role || isRoleMatch(user.role.role, item.role)))
+  }, [user])
 
   return (
     <Layout.Sider trigger={null} collapsible collapsed={collapsed}>
       <Menu theme="dark" mode="inline" selectedKeys={[location.pathname]}>
-        {pages.map(page => (
-          <Menu.Item key={page.path} icon={page.icon}>
-            <Link to={page.path}>{page.label}</Link>
+        {menu.map(item => (
+          <Menu.Item key={item.path} icon={item.icon}>
+            <Link to={item.path}>{item.label}</Link>
           </Menu.Item>
         ))}
       </Menu>
--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -1,12 +1,6 @@
-import React, { useMemo } from 'react'
+import React, { Suspense, useMemo } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Layout } from 'antd'
-import {
-  TeamOutlined,
-  SnippetsOutlined,
-  FileDoneOutlined,
-  LockOutlined
-} from '@ant-design/icons'
 import Sidebar from './Sidebar'
 import Header from './Header'
 import Loader from './Loader'
@@ -14,18 +8,24 @@ import { selectAuth } from '../store/auth/auth.slice'
 import useBoolean from '../hooks/useBoolean'
 import useTypedSelector from '../hooks/useTypedSelector'
 import storage from '../utils/storage'
+import isRoleMatch from '../utils/isRoleMatch'
 import { Roles } from '../enums/Roles'
-import { Page, ChildPage } from '../interfaces/Page'
 
+interface Page {
+  path: string
+  element: React.ReactNode
+  role?: Roles
+}
 
 const Users = React.lazy(() => import('../pages/Users'))
 const Acts = React.lazy(() => import('../pages/Acts'))
 const Agreements = React.lazy(() => import('../pages/Agreements'))
+const Agreement = React.lazy(() => import('../pages/Agreement'))
 const SkziUnits = React.lazy(() => import('../pages/SkziUnits'))
 
 const Wrapper = () => {
-  const collapsed = useBoolean(storage.get<boolean>('collapsed') || false)
   const { user } = useTypedSelector(selectAuth)
+  const collapsed = useBoolean(storage.get<boolean>('collapsed') || false)
 
   const onToggle = () => {
     collapsed.toggle()
@@ -35,59 +35,51 @@ const Wrapper = () => {
   const pages = useMemo(() => {
     const allPages: Page[] = [
       {
-        icon: <TeamOutlined />,
-        element: <Users />,
         path: '/users',
-        label: 'Пользователи',
-        roles: [Roles.System, Roles.Admin]
+        element: <Users />,
+        role: Roles.Admin
       },
       {
-        icon: <SnippetsOutlined />,
-        element: <Acts />,
         path: '/acts',
-        label: 'Акты'
+        element: <Acts />
       },
       {
-        icon: <FileDoneOutlined />,
-        element: <Agreements />,
         path: '/agreements',
-        label: 'Соглашения'
+        element: <Agreements />
       },
       {
-        icon: <LockOutlined />,
-        element: <SkziUnits />,
+        path: '/agreements/:id',
+        element: <Agreement />
+      },
+      {
         path: '/skzi-units',
-        label: 'СКЗИ'
+        element: <SkziUnits />
       }
     ]
 
-    return allPages.filter(page => user && (!page.roles?.length || page.roles.includes(user.role.role)))
+    return allPages.filter(page => user && (!page.role || isRoleMatch(user.role.role, page.role)))
   }, [user])
-
-  const createPage = (page: ChildPage) => (
-    <Route
-      key={page.path}
-      path={page.path}
-      element={
-        <React.Suspense fallback={<Loader />}>
-          {page.element}
-        </React.Suspense>
-      }
-    >
-      {page.children && page.children.map(createPage)}
-    </Route>
-  )
 
   return (
     <Layout className="wrapper">
-      <Sidebar collapsed={collapsed.value} pages={pages} />
+      <Sidebar collapsed={collapsed.value} />
 
       <Layout>
         <Header collapsed={collapsed.value} onToggle={onToggle} />
 
         <Layout.Content className="content">
           <Routes>
-            {pages.map(createPage)}
+            {pages.map(page => (
+              <Route
+                key={page.path}
+                path={page.path}
+                element={
+                  <Suspense fallback={<Loader />}>
+                    {page.element}
+                  </Suspense>
+                }
+              />
+            ))}
             <Route path="*" element={<Navigate to={pages[0].path} replace />} />
           </Routes>
         </Layout.Content>
