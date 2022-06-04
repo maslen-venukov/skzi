cd0ce0237f72e723cfe02392b68d8fd2644c4765
Sergei Venukov <shid1095@gmail.com>
2022-06-05 02:13:20 +0500

    pagination improvement

--- client/src/hooks/usePagination.ts
+++ client/src/hooks/usePagination.ts
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import { PaginationProps } from 'antd'
 import { Pagination } from '../interfaces/pagination.interface'
 
 interface PaginationParams {
   fetch: (params: Pagination) => Promise<void>
 }
 
+interface PaginationResult extends PaginationProps {
+  fetch: () => Promise<void>
+}
+
 const pageSizeOptions = [3, 6, 9]
 
-const usePagination = ({ fetch }: PaginationParams) => {
+const usePagination = ({ fetch }: PaginationParams): PaginationResult => {
   const [params, setParams] = useSearchParams()
   const [page, setPage] = useState(Number(params.get('page')) || 1)
   const [count, setCount] = useState(Number(params.get('count')) || pageSizeOptions[0])
   const location = useLocation()
   const navigate = useNavigate()
+
+  const showTotal = (total: number) => `Всего ${total}`
 
   const onChange = (page: number, count: number) => {
     setPage(page)
@@ -35,6 +42,8 @@ const usePagination = ({ fetch }: PaginationParams) => {
     pageSize: count,
     pageSizeOptions,
     showSizeChanger: true,
+    showQuickJumper: true,
+    showTotal,
     onChange,
     onShowSizeChange: onChange,
     fetch: async () => await fetch({ page, count })
